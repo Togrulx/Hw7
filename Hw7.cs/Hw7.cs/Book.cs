5c7cc1dc1566d870bbc51c1290ecb4683c894366
Togrulx <tokhrulah@code.edu.az>
2022-03-02 11:28:20 +0400

    first commit

---
+++ Hw7.cs/Hw7.cs/Book.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hw7.cs
+{
+    public class Book
+    {
+
+        public string Code { get; set; }
+        public string BookName;
+        public string AuthorName;
+        public int PageCount;
+
+        public static int _count=1;
+
+        
+        public Book(string bookname, string authorname,int pagecount)
+        {
+            BookName = bookname;
+            AuthorName = authorname;
+            PageCount = pagecount;
+
+            Code = $"{BookName[0..2].ToUpper()}{_count}";
+
+            _count++;
+        }
+
+
+        public override string ToString()
+        {
+            return $"Book Code:{Code}\n" +
+                $"Name:{BookName}\n" +
+                $"Author Name:{AuthorName}\n" +
+                $"Page Count:{PageCount}";
+        }
+
+    }
+}
---
+++ Hw7.cs/Hw7.cs/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hw7.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Library library = new Library();
+
+            library.AddBooks("Lord of the Rings ", "J.R.R. Tolkien", 400);
+            library.AddBooks("Harry Poter", " J.K. Rowling", 400);
+            library.AddBooks("Alice's Adventures in Wonderland", " Lewis Carroll", 400);
+            library.AddBooks("Pinocchio", "Carlo Collodi ", 400);
+            string aa = "400";
+
+            foreach (var item in library.SearchBooks(aa))
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+        }
+    }
+}
